298821b7bd10b0232564214364a5e95cd7cf8728
Angel <31151023+agmose@users.noreply.github.com>
2022-08-10 10:05:57 -0700

    Update solver.py

--- solver.py
+++ solver.py
@@ -1,1 +1,8 @@
+import numpy as np
 
+#We will take a linear (i.e., Ax = b) modeling approach to solve our equation :-x[0] + x[1]*ln(M) = b :
+
+M = np.log([10.80000,28.20000,70.70000,105.00000])
+A = np.vstack((-np.ones(M.size),M))
+b = np.array([23.84950,28.31647,32.59418,34.43983])
+x = np.linalg.lstsq(A.transpose(),b)[0]
